1e566202d6f5e4e56ac9c950c8abdc091e2fcc37
Bappa0507 <bappapyne2004@gmail.com>
2024-02-20 17:14:36 +0530

    bappa

---
+++ A-Note-A-Day-master/messages.js
@@ -0,0 +1,6 @@
+const anniversaryDate = 'September 17, 2017 00:00:00' 
+const noMessage = 'I LOVE YOU SO MUCH.'
+const endMessage = '<br><br><br>Love,<br>ANI'
+const messages = {
+    '10:11': 'Sample message'
+}
